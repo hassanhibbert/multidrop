7c88a8c0e1a8fe77a124d2a56fe4a316f48eff0a
H2-CODE\Hassan <hassanhibbert@gmail.com>
2017-08-13 10:16:09 -0400

    comment out Extend function

--- multidrop.js
+++ multidrop.js
@@ -12,9 +12,9 @@
    * using es2015 as well
    */
 
-  var Extend = function (source, object) {
-    return Object.assign(Object.create(source), object);
-  };
+  // var Extend = function (source, object) {
+  //   return Object.assign(Object.create(source), object);
+  // };
 
   // Private variables
   var currentPosition = 0, instanceQueue = {};
