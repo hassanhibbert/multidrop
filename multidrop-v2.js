775ca19c32c2300d47cf6ebe1c42b7aa0a04a45d
H2-CODE\Hassan <hassanhibbert@gmail.com>
2017-08-17 19:20:06 -0400

    initial setup

---
+++ multidrop-v2.js
@@ -0,0 +1,11 @@
+(function () {
+
+  // Shortcuts
+  var ObjectAssign = Object.assign;
+
+  var Extend = (source, object) => ObjectAssign(source, Object.create(object));
+
+  var Helpers = {};
+
+  var UserInterface = Extend();
+})();
