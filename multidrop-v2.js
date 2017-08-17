7e8e42ec436b423d0f807850684531d912973095
H2-CODE\Hassan <hassanhibbert@gmail.com>
2017-08-17 19:26:59 -0400

    add code to export library as a module; add additional  shortcut variables

--- multidrop-v2.js
+++ multidrop-v2.js
@@ -1,11 +1,29 @@
 (function () {
 
-  // Shortcuts
+  // Export module
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = setupValidateManager;
+    }
+    exports.MultiDrop = setupMultiDrop;
+  } else if (typeof window !== 'undefined') {
+    window.MultiDrop = setupMultiDrop;
+  }
+
+  // Shortcut variables
   var ObjectAssign = Object.assign;
+  var ObjectKeys = Object.keys;
+  var objProto = Object.prototype;
+  var toString = objProto.toString;
+  var hasOwn = objProto.hasOwnProperty;
+  var doc = document;
 
   var Extend = (source, object) => ObjectAssign(source, Object.create(object));
 
   var Helpers = {
+    isFunction(obj) {
+      return toString.call(obj) === '[object Function]';
+    },
     getElementList(elements) {
       if (typeof elements === 'string') {
         return [...elements];
@@ -18,4 +36,6 @@
   };
 
   var UserInterface = Extend();
+
+  function setupMultiDrop() {}
 })();
